7f933549bd8a25de1f31fd06c2676e73de02cdbf
andrew@gmail.com <zhangmhao@gmail.com>
2012-10-09 14:49:02 +0800

    home page is done

---
+++ app/Bootstrap.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 12-10-9
+ * Time: 下午1:40
+ * To change this template use File | Settings | File Templates.
+ */
+public class Bootstrap {
+}
